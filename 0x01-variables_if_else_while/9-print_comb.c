4b60a2bf5ad0ec4f2555d7dffa412828a133caf1
shiru-jones <winwarari@gmail.com>
2023-03-19 11:41:37 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t;
+
+	for (t = '0'; t <= '9'; t++)
+	{
+		putchar(t);
+		if (t != '9')
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
