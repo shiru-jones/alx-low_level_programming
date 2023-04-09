447475694e06abed560e71d8859fba1246762039
shiru-jones <winwarari@gmail.com>
2023-04-09 06:26:11 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry  point
+ * @argc: arg count
+ * @argv: array of pointers vector
+ *
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
