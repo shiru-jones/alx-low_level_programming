791a606dbf6773199da24addde3d80dbb71ea7e9
shiru-jones <winwarari@gmail.com>
2023-04-09 06:30:19 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry  point
+ * @argv: argument
+ * @argc: argument
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
