b41b8135e5b27b9f261883e390ceb1014f8ece67
shiru-jones <winwarari@gmail.com>
2023-04-10 04:40:02 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+ void _puts(char *s);
+
+{
+	void _puts(char *s);
+
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
